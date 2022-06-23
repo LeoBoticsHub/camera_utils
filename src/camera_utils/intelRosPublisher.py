ad9f041362a568a65dc6b7e746c2a053ec144fb8
Federico Rollo <rollo.f96@gmail.com>
2022-06-23 12:54:13 +0200

    added intel camera ros publisher file

---
+++ src/camera_utils/intelRosPublisher.py
@@ -0,0 +1,24 @@
+import time
+from camera_wrapper_node import IntelRosWrapper
+import select
+import sys
+
+# function used to stop loop functions
+def stop_loop(stop_entry: str) -> bool:
+    '''
+    Used to quit an infinite loop with a char/string entry
+    '''
+    rlist = select.select([sys.stdin], [], [], 0.001)[0]
+    if rlist and sys.stdin.readline().find(stop_entry) != -1:
+        return True
+    return False
+
+if __name__ == "__main__":
+
+    camera = IntelRosWrapper()
+
+    while not stop_loop('q'):
+
+        camera.get_aligned_frames()
+        camera.get_intrinsics()
+
