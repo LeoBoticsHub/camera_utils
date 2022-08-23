282821713bf2dca7eb75e055311ef6a625f69eb7
Federico Rollo <rollo.f96@gmail.com>
2022-08-23 13:37:17 +0200

    added setup file for pip installation

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
 
-d = generate_distutils_setup(
-    packages=['camera_utils'],
-    package_dir={'': 'src'}
+package_name = 'camera_utils'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=[package_name],
+    data_files=[],
+    install_requires=['setuptools', 'numpy', 'open3d', 'pyrealsense2', 'imutils'],
+    zip_safe=True,
+    maintainer='frollo',
+    maintainer_email='rollo.f96@gmail.com',
+    description='This package contains functions useful for camera use and other computer vision computations',
+    license='GNU GENERAL PUBLIC LICENSE v3',
+    entry_points={},
 )
-setup(**d)
