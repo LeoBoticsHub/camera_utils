1909772769f1781028bfa6d1e5c0495774697678
Federico Rollo <rollo.f96@gmail.com>
2022-09-21 11:07:35 +0200

    added meta-data to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 
 package_name = 'camera_utils'
 
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
 setup(
     name=package_name,
     version='1.0.0',
@@ -9,9 +12,13 @@ setup(
     data_files=[],
     install_requires=['setuptools', 'numpy', 'open3d', 'pyrealsense2', 'imutils'],
     zip_safe=True,
+    url='https://github.com/IASRobolab/camera_utils',
     maintainer='frollo',
     maintainer_email='rollo.f96@gmail.com',
     description='This package contains functions useful for camera use and other computer vision computations',
     license='GNU GENERAL PUBLIC LICENSE v3',
-    entry_points={},
+    # entry_points={},
+    # extras_require = { 'dev': ['pytest>=3.7']}, # to install with this run 'pip install -e .[dev]
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
